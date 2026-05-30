a1b660b898e0e18b50fa05f8d2dbc516943eef6e
𝖙𝖍𝖔𝖒𝖆𝖟 𝖉𝖊 𝖏𝖊𝖘𝖚𝖘 <enzo.jesus@aluno.senai.br>
2026-05-30 17:48:36 -0300

    Add CardJogo export to components index

--- rn-navigation-lab-main/src/components/index.js
+++ rn-navigation-lab-main/src/components/index.js
@@ -1,6 +1,6 @@
 // Exportacoes centralizadas dos componentes (barrel export)
-// Importar de um unico lugar: import { BotaoAcao } from '../components';
-export { default as BotaoAcao } from './BotaoAcao';
+// Importar de um unico lugar:
+// import { BotaoAcao, CardJogo } from '../components';
 
-// TODO: apos criar CardJogo.js, adicione a linha abaixo:
-// export { default as CardJogo } from './CardJogo';
+export { default as BotaoAcao } from './BotaoAcao';
+export { default as CardJogo } from './CardJogo';
